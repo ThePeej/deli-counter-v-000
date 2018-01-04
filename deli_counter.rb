05dce4a2146afdba946e796ce15580278472217c
PJ Frias <peejfrias@gmail.com>
2018-01-04 01:02:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(cust_array)
+  message = ""
+  if (cust_array.size == 0)
+    puts "The line is currently empty"
+  else
+    cust_array.each_with_index do |customer, index|
+      message = "The line is currently:"
+      position = index + 1
+      message << " #{position}. #{customer}"
+    end
+  end
+  message
+end
+      
